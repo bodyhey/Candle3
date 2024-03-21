0e11c68e1c28e48689d6bd021bbc15ddf16c3b05
etet100 <etet100>
2024-03-21 22:05:30 +0100

    GRBL configurator working more or less

--- src/candle/frmgrblconfigurator.cpp
+++ src/candle/frmgrblconfigurator.cpp
@@ -12,143 +12,191 @@
 #include <CListProperty.h>
 #include <CFontProperty.h>
 
+enum Group {
+    General,
+    StepInvert,
+    DirInvert,
+    Limits,
+    Homing,
+    HomingInvert,
+    Spindle,
+    Steps,
+    MaxRate,
+    Acceleration,
+    Reporting,
+    MaxTravel,
+    Quality,
+};
+
+struct ConfigGroup {
+    Group group;
+    QByteArray name;
+    std::vector<ConfigEntry> entries;
+    CPropertyHeader* header = nullptr;
+};
+
+#define REPORT_POS_TYPE_SETTING_ID 150
+#define REPORT_POS_TYPE_SETTING_BIT 1
+#define REPORT_BUFFER_SETTING_ID 151
+#define REPORT_BUFFER_SETTING_BIT 2
+
+std::vector<ConfigGroup> ConfigMap = {
+    {
+        General,
+        "General settings",
+        {
+            {0, "Step pulse, microseconds", "Shortest pulses for stepper drivers to recognize.", Integer},
+            {1, "Step idle delay, milliseconds", "Delay before disabling steppers after motion.", Integer},
+            {32, "Laser mode", "Enables continuous motion for laser cutting.", Boolean},
+            //uCNC uses mask??
+            {4, "Step enable invert", "Inverts the stepper enable pin.", Boolean},
+            //uCNC uses mask??
+            {5, "Limit pins invert", "Inverts the limit pins.", Boolean},
+            {6, "Probe pin invert", "Inverts the probe pin.", Boolean},
+        }
+    },
+    {
+        Reporting,
+        "Reporting machine status",
+        {
+             {REPORT_POS_TYPE_SETTING_ID , "Report position type", "Enabled `MPos`, Disabled `WPos`", Boolean},
+             {REPORT_BUFFER_SETTING_ID, "Report buffer data", "Enabled `Buf` field appears with planner and serial RX available buffer.", Boolean},
+             {13, "Report inches", "Toggles between mm and inches for reporting.", Boolean},
+        }
+    },
+    {
+        Quality,
+        "Quality",
+        {
+            {11, "Junction deviation, mm", "Determines cornering speed.", Double},
+            {12, "Arc tolerance, mm", "Accuracy of arc tracing.", Double},
+        }
+    },
+    {
+        Limits,
+        "Limits",
+        {
+             {20, "Soft limits", "Prevents machine from traveling beyond limits.", Boolean},
+             {21, "Hard limits", "Uses physical switches to prevent over-travel.", Boolean},
+        },
+    },
+    {
+        StepInvert,
+        "Step signal polarity",
+        {
+            {2, "%1 step signal invert", "Inverts the step pulse signal.", Axes},
+        }
+    },
+    {
+        DirInvert,
+        "Dir signal polarity",
+        {
+            {3, "%1 direction signal invert", "Inverts the direction signal for each axis.", Axes},
+        }
+    },
+    {
+        Homing,
+        "Homing settings",
+        {
+            {22, "Homing cycle", "Locates a consistent position on startup.", Boolean},
+            {24, "Homing feed, mm/min", "Feed rate for precise homing location.", Integer},
+            {25, "Homing seek, mm/min", "Search rate for homing cycle.", Integer},
+            {26, "Homing debounce, milliseconds", "Debounce delay for homing switches.", Integer},
+            {27, "Homing pull-off, mm", "Distance to pull off from limit switches after homing.", Integer},
+        }
+    },
+    {
+        HomingInvert,
+        "Homing signal polarity",
+        {
+            {23, "%1 homing dir invert", "Inverts homing direction for certain axes.", Axes},
+        }
+    },
+    {
+        Spindle,
+        "Spindle",
+        {
+            {30, "Max spindle speed, RPM", "Sets max spindle speed for 5V PWM output.", Integer},
+            {31, "Min spindle speed, RPM", "Sets min spindle speed for 0.02V PWM output.", Integer},
+        }
+    },
+    {
+        Steps,
+        "Steps per mm",
+        {
+            {100, "X steps/mm", "Steps per mm for X axis.", Integer},
+            {101, "Y steps/mm", "Steps per mm for Y axis.", Integer},
+            {102, "Z steps/mm", "Steps per mm for Z axis.", Integer},
+        }
+    },
+    {
+        MaxRate,
+        "Max axis rate",
+        {
+            {110, "X max rate, mm/min", "Max rate X axis can move.", Integer},
+            {111, "Y max rate, mm/min", "Max rate Y axis can move.", Integer},
+            {112, "Z max rate, mm/min", "Max rate Z axis can move.", Integer},
+        }
+    },
+    {
+        Acceleration,
+        "Max axis acceleration",
+        {
+            {120, "X acceleration, mm/sec^2", "Acceleration parameters for X axis.", Integer},
+            {121, "Y acceleration, mm/sec^2", "Acceleration parameters for Y axis.", Integer},
+            {122, "Z acceleration, mm/sec^2", "Acceleration parameters for Z axis.", Integer},
+        }
+    },
+    {
+        MaxTravel,
+        "Max axis travel",
+        {
+            {130, "X max travel, mm", "Max travel for X axis.", Integer},
+            {131, "Y max travel, mm", "Max travel for Y axis.", Integer},
+            {132, "Z max travel, mm", "Max travel for Z axis.", Integer},
+        }
+    }
+};
+
 frmGrblConfigurator::frmGrblConfigurator(QWidget *parent, Communicator *communicator)
     : QDialog(parent)
     , ui(new Ui::frmGrblConfigurator)
     , m_communicator(communicator)
 {
     ui->setupUi(this);
-
     ui->editor->init();
 
-    CPropertyHeader* dirInvertHead = new CPropertyHeader("DirInvert", "Invert axis direction");
-    ui->editor->add(dirInvertHead);
+    for (std::vector<ConfigGroup>::iterator it = ConfigMap.begin(); it != ConfigMap.end(); it++) {
+        ConfigGroup &group = *it;
+        CPropertyHeader* header = new CPropertyHeader(group.name, group.name);
 
-    ui->editor->add(new CBoolProperty(dirInvertHead, "dirInvertX", "X", false));
-    ui->editor->add(new CBoolProperty(dirInvertHead, "dirInvertY", "Y", false));
-    ui->editor->add(new CBoolProperty(dirInvertHead, "dirInvertZ", "Z", false));
-    ui->editor->add(new CBoolProperty(dirInvertHead, "dirInvertA", "A", false));
-    ui->editor->add(new CBoolProperty(dirInvertHead, "dirInvertB", "B", false));
+        group.header = header;
+        ui->editor->add(header);
 
-    CPropertyHeader* stepsPerUnitHead = new CPropertyHeader("StepsPerUnit", "Steps per mm");
-    ui->editor->add(stepsPerUnitHead);
-
-    ui->editor->add(new CDoubleProperty(stepsPerUnitHead, "stepsPerUnitX", "X", 80));
-    ui->editor->add(new CDoubleProperty(stepsPerUnitHead, "stepsPerUnitY", "Y", 80));
-    ui->editor->add(new CDoubleProperty(stepsPerUnitHead, "stepsPerUnitZ", "Z", 400));
-    ui->editor->add(new CDoubleProperty(stepsPerUnitHead, "stepsPerUnitA", "A", 100));
-    ui->editor->add(new CDoubleProperty(stepsPerUnitHead, "stepsPerUnitB", "B", 100));
-
-    // Color properties
-    CPropertyHeader* colorhead = new CPropertyHeader("ColorHead", "Color Properties");
-    ui->editor->add(colorhead);
-
-    CColorProperty* c1 = new CColorProperty(colorhead, "Color1", "Color 1 (user editable)", Qt::red);
-    ui->editor->add(c1);
-
-    CColorProperty* c2 = new CColorProperty(colorhead, "Color2", "Color 2 (not user editable)", Qt::green);
-    ui->editor->add(c2);
-    c2->allowListColorsOnly(true);
-
-    CColorProperty* c3 = new CColorProperty(colorhead, "Color3", "Color 3 (Base colors)", QColor("#345678"));
-    ui->editor->add(c3);
-    //c3->allowListColorsOnly(true);
-    c3->setColorsList(QColorComboBox::baseColors());
-
-    // Boolean properties
-    CPropertyHeader* bhead = new CPropertyHeader("BoolHead", "Boolean Properties");
-    ui->editor->add(bhead);
-
-    CBoolProperty* b1 = new CBoolProperty(bhead, "SomeBool1", "Boolean 1 On", true);
-    ui->editor->add(b1);
-    b1->setMarked(Qt::Unchecked);
-
-    CBoolProperty* b2 = new CBoolProperty(b1, "SomeBool2", "Boolean 2 Off", false);
-    ui->editor->add(b2);
-    b2->setBackground(Qt::yellow);
-
-    CBoolProperty* b3 = new CBoolProperty(bhead, "SomeBool3", "Boolean 3 Disabled", false);
-    ui->editor->add(b3);
-    b3->setDisabled(true);
-
-    CBoolProperty* b4 = new CBoolProperty(bhead, "SomeBool4", "Boolean 4 On", true);
-    ui->editor->add(b4);
-    b4->setMarked(Qt::Checked);
-
-    CBoolProperty* b5 = new CBoolProperty(bhead, "SomeBool5", "Boolean 5 Off", false);
-    ui->editor->add(b5);
-    b5->setMarked(Qt::PartiallyChecked);
-
-
-    // Integer properties
-    CPropertyHeader* inthead = new CPropertyHeader("IntHead", "Integer Properties");
-    ui->editor->add(inthead);
-
-    CIntegerProperty* i1 = new CIntegerProperty(inthead, "SomeInt1", "Integer 1", 123, 0);
-    ui->editor->add(i1);
-    i1->setBackground(Qt::cyan);
-
-    CIntegerProperty* i2 = new CIntegerProperty(i1, "SomeInt2", "Integer 2 (-100..100)", -40, 0, -100, 100);
-    ui->editor->add(i2);
-    i2->setMarked(Qt::Checked);
-
-
-    // Double properties
-    CPropertyHeader* dblhead = new CPropertyHeader("DblHead", "Double Properties");
-    ui->editor->add(dblhead);
-
-    CDoubleProperty* d1 = new CDoubleProperty(dblhead, "SomeDouble1", "Double 1", 45.639, 0, -15.93, 1378789123232.327878273);
-    ui->editor->add(d1);
-
-
-    // Text properties
-    CPropertyHeader* txthead = new CPropertyHeader("TxtHead", "Textual Properties");
-    ui->editor->add(txthead);
-
-    CStringProperty* s1 = new CStringProperty(txthead, "SomeString1", "A String", "the sun is shining :)");
-    ui->editor->add(s1);
-
-
-    // List properties
-    CPropertyHeader* listhead = new CPropertyHeader("ListHead", "List Properties");
-    listhead->setMarked(true);
-    ui->editor->add(listhead);
-
-    CListData list1;
-    list1 << CListDataItem("Item 1", QIcon(":/Info"))
-          << CListDataItem("Item 2")
-          << CListDataItem("Item 3", QIcon(":/Open"))
-          << CListDataItem("Item 4", QIcon(":/Remove"))
-          << CListDataItem("Item 5");
-
-    CListProperty* l1 = new CListProperty(listhead, "List1", "List 1", list1, 0);
-    ui->editor->add(l1);
-    l1->setBackground(Qt::magenta);
-    l1->setTextColor(Qt::green);
-    l1->setMarked(Qt::Checked);
-
-    CListProperty* l2 = new CListProperty(listhead, "List2", "List 2 (shared List 1)", list1, 5);
-    ui->editor->add(l2);
-    l2->setMarked(Qt::Unchecked);
-
-
-    // Font properties
-    CPropertyHeader* fonthead = new CPropertyHeader("FontHead", "Font Properties");
-    ui->editor->add(fonthead);
-
-    QFont font1("Arial", 10, 100, true);
-    CFontProperty* f1 = new CFontProperty(fonthead, "Font1", "Font 1", font1);
-    ui->editor->add(f1);
-
-    QFont font2("Courier", 12, 1, false);
-    CFontProperty* f2 = new CFontProperty(fonthead, "Font2", "Font 2", font2);
-    ui->editor->add(f2);
+        for (std::vector<ConfigEntry>::iterator it2 = group.entries.begin(); it2 != group.entries.end(); it2++) {
+            ConfigEntry &entry = *it2;
+            switch (entry.type) {
+                case Axes:
+                    entry.properties = addAxesProperty(header, entry);
+                    break;
+                case Boolean:
+                    entry.property = addBooleanProperty(header, entry);
+                    break;
+                case Integer:
+                    entry.property = addIntegerProperty(header, entry);
+                    break;
+                case Double:
+                    entry.property = addDoubleProperty(header, entry);
+                    break;
+            }
+        }
+    }
 
     ui->editor->adjustToContents();
 
-    connect(m_communicator, SIGNAL(settingsReceived(QMap<int, double>)), this, SLOT(setSettings(QMap<int, double>)));
-    m_communicator->sendCommand("$$");
+    connect(ui->editor, SIGNAL(itemChanged(QTreeWidgetItem*,int)), this, SLOT(itemChanged(QTreeWidgetItem*,int)));
 
+    update();
 }
 
 frmGrblConfigurator::~frmGrblConfigurator()
@@ -158,5 +206,234 @@ frmGrblConfigurator::~frmGrblConfigurator()
 
 void frmGrblConfigurator::setSettings(QMap<int, double> settings)
 {
+    disconnect(m_communicator, SIGNAL(settingsReceived(QMap<int,double>)), this, SLOT(setSettings(QMap<int,double>)));
+
+    if (m_isSaving) {
+        m_isSaving = false;
+        findParametersToBeSaved(settings);
+
+        return;
+    }
+
+
+    m_currentSettings = settings;
+
+    setInfo("Updated", Qt::black);
     qDebug() << "Settings received" << settings;
+
+    // ugly hack! split status report setting into two separate settings
+    int statusReportSetting = settings[10];
+    settings[REPORT_BUFFER_SETTING_ID] = statusReportSetting & REPORT_BUFFER_SETTING_BIT;
+    settings[REPORT_POS_TYPE_SETTING_ID] = statusReportSetting & REPORT_POS_TYPE_SETTING_BIT;
+    //
+
+    for (std::vector<ConfigGroup>::iterator it = ConfigMap.begin(); it != ConfigMap.end(); it++) {
+        ConfigGroup &group = *it;
+        for (std::vector<ConfigEntry>::iterator it2 = group.entries.begin(); it2 != group.entries.end(); it2++) {
+            ConfigEntry entry = *it2;
+
+            double setting = settings[entry.index];
+
+            QMap<Axis, CBaseProperty*> properties;
+            switch (entry.type) {
+                case Axes:
+                    properties = entry.properties;
+                    (dynamic_cast<CBoolProperty*>(properties[X]))->setValue(((int) setting) & 1);
+                    (dynamic_cast<CBoolProperty*>(properties[Y]))->setValue(((int) setting) & 2);
+                    (dynamic_cast<CBoolProperty*>(properties[Z]))->setValue(((int) setting) & 4);
+                    break;
+                case Boolean:
+                    CBoolProperty *boolProperty;
+                    boolProperty = dynamic_cast<CBoolProperty*>(entry.property);
+                    boolProperty->setValue((bool) setting);
+                    break;
+                case Integer:
+                    CIntegerProperty *intProperty;
+                    intProperty = dynamic_cast<CIntegerProperty*>(entry.property);
+                    intProperty->setValue((int) setting);
+                    break;
+                case Double:
+                    CDoubleProperty *doubleProperty;
+                    doubleProperty = dynamic_cast<CDoubleProperty*>(entry.property);
+                    doubleProperty->setValue(setting);
+                    break;
+            }
+        }
+    }
+}
+
+void frmGrblConfigurator::onUpdateClicked()
+{
+    update();
+}
+
+void frmGrblConfigurator::itemChanged(QTreeWidgetItem *item, int column)
+{
+    Q_UNUSED(column);
+
+    CBaseProperty *property = dynamic_cast<CBaseProperty*>(item);
+
+    int entryIndex = property->data(0, Qt::UserRole).toInt();
+    Axis axis = Axis::None;
+    if (entryIndex > 1000) {
+        axis = Axis(entryIndex % 10000);
+        entryIndex = entryIndex / 10000;
+    }
+
+    if (entryIndex == REPORT_POS_TYPE_SETTING_ID) {
+        setSettingsBit(10, REPORT_POS_TYPE_SETTING_BIT, property->getVariantValue().toBool());
+    } else
+    if (entryIndex == REPORT_BUFFER_SETTING_ID) {
+        setSettingsBit(10, REPORT_BUFFER_SETTING_BIT, property->getVariantValue().toBool());
+    } else
+    if (axis != Axis::None) {
+        setSettingsBit(entryIndex, 1 << axis, property->getVariantValue().toBool());
+    } else {
+        m_currentSettings[entryIndex] = property->getVariantValue().toDouble();
+    }
+}
+
+void frmGrblConfigurator::update()
+{
+    setInfo("Updating...", Qt::red);
+
+    connect(m_communicator, SIGNAL(settingsReceived(QMap<int,double>)), this, SLOT(setSettings(QMap<int,double>)));
+    QTimer::singleShot(200, this, [this]() {
+        m_communicator->sendCommand("$$");
+    });
+}
+
+void frmGrblConfigurator::setInfo(QString text, QColor color)
+{
+    ui->lblInfo->setText(text);
+    ui->lblInfo->setStyleSheet(QString("color: %1").arg(color.name()));
+}
+
+QMap<Axis, CBaseProperty*> frmGrblConfigurator::addAxesProperty(CPropertyHeader *header, ConfigEntry entry)
+{
+    return {
+        {X, addAxisProperty(header, entry, Axis::X)},
+        {Y, addAxisProperty(header, entry, Axis::Y)},
+        {Z, addAxisProperty(header, entry, Axis::Z)},
+    };
+}
+
+CBaseProperty* frmGrblConfigurator::addAxisProperty(CPropertyHeader *header, ConfigEntry entry, Axis axis)
+{
+    // @TODO ...
+    QMap<Axis, QString> axisNames = {
+        {X, "X"},
+        {Y, "Y"},
+        {Z, "Z"},
+    };
+
+    CBoolProperty* property = new CBoolProperty(
+        header,
+        QByteArray::fromStdString(QString("#%1_%2").arg(entry.index).arg(axis).toStdString()),
+        entry.description.arg(axisNames[axis]),
+        0,
+        0
+    );
+    property->setData(0, Qt::UserRole, entry.index * 10000 + axis);
+
+    ui->editor->add(property);
+
+    return property;
+}
+
+void frmGrblConfigurator::setSettingsBit(int index, int bit, bool bitValue)
+{
+    int value = m_currentSettings[index];
+    if (bitValue) {
+        value |= bit;
+    } else {
+        value &= ~bit;
+    }
+    m_currentSettings[index] = value;
+}
+
+void frmGrblConfigurator::accept()
+{
+    if (QMessageBox::question(this, "Saving settings", "Are you sure you want to update machine settings?") == QMessageBox::No) {
+        return;
+    }
+
+    m_isSaving = true;
+    update();
+}
+
+void frmGrblConfigurator::findParametersToBeSaved(QMap<int, double> settings)
+{
+    QMap<int, double> toBeSaved = {};
+    for (QMap<int, double>::iterator it = m_currentSettings.begin(); it != m_currentSettings.end(); it++) {
+        int index = it.key();
+        double value = it.value();
+
+        if (settings[index] != value) {
+            toBeSaved[index] = value;
+        }
+    }
+
+    if (toBeSaved.size() == 0) {
+        QDialog::accept();
+
+        return;
+    }
+
+    for (QMap<int, double>::iterator it = toBeSaved.begin(); it != toBeSaved.end(); it++) {
+        QString command = QString("$%1=%2").arg(it.key()).arg(it.value());
+        qDebug() << m_communicator->sendCommand(command);
+        qDebug() << "Sending command" << command;
+    }
+
+    QDialog::accept();
+}
+
+CBaseProperty* frmGrblConfigurator::addBooleanProperty(CPropertyHeader *header, ConfigEntry entry)
+{
+    CBoolProperty* property = new CBoolProperty(
+        header,
+        QByteArray::fromStdString(QString("#%1").arg(entry.index).toStdString()),
+        entry.description,
+        0,
+        0
+    );
+    property->setData(0, Qt::UserRole, entry.index);
+
+    ui->editor->add(property);
+
+    return property;
+}
+
+CBaseProperty* frmGrblConfigurator::addIntegerProperty(CPropertyHeader *header, ConfigEntry entry)
+{
+    CIntegerProperty* property = new CIntegerProperty(
+        header,
+        QByteArray::fromStdString(QString("#%1").arg(entry.index).toStdString()),
+        entry.description,
+        0,
+        0
+    );
+    property->setData(0, Qt::UserRole, entry.index);
+
+    ui->editor->add(property);
+
+    return property;
+}
+
+CBaseProperty *frmGrblConfigurator::addDoubleProperty(CPropertyHeader *header, ConfigEntry entry)
+{
+    CDoubleProperty* property = new CDoubleProperty(
+        header,
+        QByteArray::fromStdString(QString("#%1").arg(entry.index).toStdString()),
+        entry.description,
+        0,
+        0
+    );
+
+    property->setData(0, Qt::UserRole, entry.index);
+
+    ui->editor->add(property);
+
+    return property;
 }
--- src/candle/frmgrblconfigurator.h
+++ src/candle/frmgrblconfigurator.h
@@ -3,10 +3,35 @@
 
 #include <QDialog>
 #include "communicator.h"
+#include <CPropertyHeader.h>
+#include <CBaseProperty.h>
 
 namespace Ui {
 class frmGrblConfigurator;
 }
+
+enum Type {
+    Boolean,
+    Integer,
+    Double,
+    Axes,
+};
+
+enum Axis {
+    None = -1,
+    X = 0,
+    Y = 1,
+    Z = 2,
+};
+
+struct ConfigEntry {
+    int index;
+    QString description;
+    QString meaning;
+    Type type;
+    CBaseProperty *property = nullptr;
+    QMap<Axis, CBaseProperty*> properties = {};
+};
 
 class frmGrblConfigurator : public QDialog
 {
@@ -16,12 +41,26 @@ class frmGrblConfigurator : public QDialog
         explicit frmGrblConfigurator(QWidget *parent, Communicator *communicator);
         ~frmGrblConfigurator();
 
-    private:
+    private:        
         Ui::frmGrblConfigurator *ui;
         Communicator *m_communicator;
+        QMap<int, double> m_currentSettings;
+        bool m_isSaving = false;
+        void setInfo(QString text, QColor color);
+        QMap<Axis, CBaseProperty*> addAxesProperty(CPropertyHeader *, ConfigEntry);
+        CBaseProperty* addBooleanProperty(CPropertyHeader *, ConfigEntry);
+        CBaseProperty* addIntegerProperty(CPropertyHeader *, ConfigEntry);
+        CBaseProperty* addDoubleProperty(CPropertyHeader *, ConfigEntry);
+        CBaseProperty* addAxisProperty(CPropertyHeader *header, ConfigEntry entry, Axis axis);
+        void setSettingsBit(int, int, bool);
+        void accept() override;
+        void findParametersToBeSaved(QMap<int, double>);
 
     private slots:
         void setSettings(QMap<int, double>);
+        void update();
+        void onUpdateClicked();
+        void itemChanged(QTreeWidgetItem *item, int column);
 };
 
 #endif // FRMGRBLCONFIGURATOR_H
